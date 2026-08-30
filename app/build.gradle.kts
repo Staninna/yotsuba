9004d59598004daf6eb8ee1af97e018ed3627f38
Staninna <26458805+Staninna@users.noreply.github.com>
2026-08-30 05:14:16 +0200

    Bump to 2.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "dev.stan.yotsuba"
         minSdk = 26
         targetSdk = 37
-        versionCode = 12
-        versionName = "2.0.0"
+        versionCode = 13
+        versionName = "2.1.0"
         testInstrumentationRunner = "dev.stan.yotsuba.HiltTestRunner"
     }
 
